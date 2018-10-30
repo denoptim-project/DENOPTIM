30a72e79606777144d44cbc4918c433fe8353fd7
Marco.Foscato <mfo051@uib.no>
2018-10-30 14:19:22 +0100

    rewriting comparator

--- src/DENOPTIM/src/utils/ConnectedLigandComparator.java
+++ src/DENOPTIM/src/utils/ConnectedLigandComparator.java
@@ -23,55 +23,36 @@ public class ConnectedLigandComparator implements Comparator<ConnectedLigand>
         int cnnA = a.getConnections();
         int cnnB = b.getConnections();
 
-        //Get the masses
-        int massA;
-        int massB;
-        if (!a.isDummy() && !b.isDummy())
+        //Get the masses and #connection (fictitious for dummy)
+        int massA = 1; 
+        int massB = 1; 
+        if (a.isDummy())
         {
+            if (cnnA == 1)
+            {
+                cnnA = 100;
+            }
+	}
+	else
+	{
             massA = a.getAtom().getMassNumber();
-            massB = b.getAtom().getMassNumber();
+        }
+
+        if (b.isDummy())
+        {
+            if (cnnB == 1)
+            {
+                cnnB = 100;
+            }
         }
         else
         {
-	    // For dummy atoms set fictitious mass and connectivity
-            if (a.isDummy())
-            {
-                massA = 1;
-                if (cnnA == 1)
-                {
-                    cnnA = 100;
-                }
-            }
-            if (b.isDummy())
-            {
-                massB = 1;
-                if (cnnB == 1)
-                {
-                    cnnB = 100;
-                }
-            }
+            massB = b.getAtom().getMassNumber();
         }
 
         //Decide on priority
         if (cnnA == cnnB)
         {
-            if (a.isDummy())
-            {
-                massA = 1;
-            }
-            else
-            {
-                massA = a.getAtom().getMassNumber();
-            }
-            if (b.isDummy())
-            {
-                massB = 1;
-            }
-            else
-            {
-                massB = b.getAtom().getMassNumber();
-            }
-
             if (massA == massB)
             {
                 return EQUAL;
