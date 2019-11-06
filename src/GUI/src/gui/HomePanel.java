fd01e63210f8fdcf933de1ad54841bea3de32bd7
Marco.Foscato <mfo051@uib.no>
2019-11-06 14:34:46 +0100

    project prepared for eclipse

--- src/GUI/src/gui/HomePanel.java
+++ src/GUI/src/gui/HomePanel.java
@@ -6,7 +6,6 @@ import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
