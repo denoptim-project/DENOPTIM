93d89ade6d99c79c2dcb02e8eb10c002dd9c5be2
Marco.Foscato <mfo051@uib.no>
2020-02-25 21:55:11 +0100

    Making playground for development of graph template

---
+++ test/functional_tests/t99/runt99.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+wrkDir=`pwd`
+logFile="t99.log"
+paramFile="t99.params"
+
+mv data/* "$wrkDir"
+rm -rf data
+
+#Adjust path in scripts and parameter files
+filesToModify=$(find . -type f | xargs grep -l "OTF")
+for f in $filesToModify
+do
+    sed "$sedInPlace" "s|OTF_WDIR|$wrkDir|g" "$f"
+    sed "$sedInPlace" "s|OTF_PROCS|$DENOPTIMslaveCores|g" "$f"
+done
+
+#Run it
+exec 6>&1
+exec > "$logFile"
+exec 2>&1
+"$javaDENOPTIM" -jar "$DENOPTIMJarFiles/DenoptimGA.jar" "$paramFile"
+exec 1>&6 6>&- 
+
+#Check outcome
+echo " "
+echo "WARNING: We do not yet check the outcome of this test run."
+echo " "
+
+exit 0
