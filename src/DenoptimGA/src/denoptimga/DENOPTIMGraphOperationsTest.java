471c0754c68c64bc110012e8339a65e82eae3e3c
Einar Kjellback <einarkjellback@fastmail.com>
2021-04-17 11:38:59 +0200

    Made a failing unit test

--- src/DenoptimGA/src/denoptimga/DENOPTIMGraphOperationsTest.java
+++ src/DenoptimGA/src/denoptimga/DENOPTIMGraphOperationsTest.java
@@ -1,12 +1,24 @@
 package denoptimga;
 
 import denoptim.exception.DENOPTIMException;
+import denoptim.fragspace.FragmentSpace;
 import denoptim.molecule.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
 
+import javax.vecmath.Point3d;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static denoptim.molecule.DENOPTIMEdge.*;
 import static denoptim.molecule.DENOPTIMVertex.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +29,21 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 public class DENOPTIMGraphOperationsTest {
+
+    IChemObjectBuilder chemBuilder = DefaultChemObjectBuilder.getInstance();
+    private final Random rng = new Random();
+    private static APClass DEFAULT_APCLASS;
+
+//------------------------------------------------------------------------------
+
+    @BeforeAll
+    static void setUpClass() {
+        try {
+            DEFAULT_APCLASS = APClass.make("norule", 0);
+        } catch (DENOPTIMException e) {
+            e.printStackTrace();
+        }
+    }
 
 //------------------------------------------------------------------------------
 
@@ -160,6 +187,225 @@ public class DENOPTIMGraphOperationsTest {
 
 //------------------------------------------------------------------------------
 
+    @Test
+    public void testExtractPattern_fusedRings() {
+        ExtractPatternCase testCase = getFusedRings();
+
+        List<DENOPTIMGraph> subgraphs = DENOPTIMGraphOperations
+                .extractPattern(testCase.g, GraphPattern.RING);
+
+        assertTrue(testCase.matchesExpected(subgraphs));
+    }
+
+//------------------------------------------------------------------------------
+
+    /**
+     * Returns a molecule consisting of two pairs of fused rings connected
+     * by an oxygen atom. The dots represents chords. The molecule looks as
+     * follows:
+     *     Cl
+     *     |   ↑
+     * O - C - C →       ← N - |
+     * .   |   .       ↑   .   |
+     * .   N - C - O - C - C - C →
+     * .   |   .       |   .   ↓
+     * . . N . .       O . .
+     *
+     * The atoms are labelled in order of the leftmost then topmost.
+     *     1
+     *     |   ↑
+     * 0 - 2 - 5 →        ← 10 - |
+     * .   |   .       ↑    .    |
+     * .   3 - 6 - 7 - 8 - 11 - 12 →
+     * .   |   .       |   .     ↓
+     * . . 4 . .       9 . .
+     */
+    private ExtractPatternCase getFusedRings() {
+        Function<String, DENOPTIMVertex> vertexSupplier = s -> {
+            int apCount = 0;
+            switch (s) {
+                case "Cl":
+                    apCount = 1;
+                    break;
+                case "O":
+                    apCount = 2;
+                    break;
+                case "N":
+                    apCount = 3;
+                    break;
+                case "C":
+                    apCount = 4;
+                    break;
+            }
+            return buildFragment(s, apCount);
+        };
+
+        /* We label the vertices in order of top left to bottom right. */
+        List<DENOPTIMVertex> vertices = Stream.of(
+                "O",
+                "Cl", "C", "N", "N",
+                "C", "C",
+                "O",
+                "C", "O",
+                "N", "C",
+                "C"
+        ).map(vertexSupplier).collect(Collectors.toList());
+
+        /* Here we specify the connections between atoms. Entry i lists
+        vertex i's neighbors. Entries are separated by a -1. Previously
+        connected vertices are not connected twice. Chords are not connected. */
+        List<Integer> edges = Arrays.asList(
+                2, -1,
+                2, -1,
+                3, 5, -1,
+                4, 6, -1,
+                -1,
+                -1,
+                7, -1,
+                8, -1,
+                9, 11, -1,
+                -1,
+                12, -1,
+                12, -1
+        );
+
+        DENOPTIMGraph g = buildGraph(vertices, edges);
+        g.renumberGraphVertices();
+        DENOPTIMGraph.setScaffold(vertices.get(0));
+        addRings(vertices, g);
+        Set<DENOPTIMGraph> expectedSubgraphs = getExpectedSubgraphs(vertices, g);
+        return new ExtractPatternCase(g, 2, expectedSubgraphs);
+    }
+
+//------------------------------------------------------------------------------
+
+    private DENOPTIMGraph buildGraph(List<DENOPTIMVertex> vertices,
+                                     List<Integer> edges) {
+        DENOPTIMGraph g = new DENOPTIMGraph();
+        g.addVertex(vertices.get(0));
+
+        HashMap<String, BondType> bondMap = new HashMap<>();
+        bondMap.put(String.valueOf(DEFAULT_APCLASS), BondType.SINGLE);
+        FragmentSpace.setBondOrderMap(bondMap);
+
+        for (int j = 0, i = 0; j < edges.size(); j++) {
+            int adj = edges.get(j);
+            if (adj == -1) {
+                i++;
+            } else {
+                DENOPTIMVertex srcVertex = vertices.get(i);
+                DENOPTIMVertex trgVertex = vertices.get(adj);
+
+                DENOPTIMAttachmentPoint srcAP = srcVertex
+                        .getAttachmentPoints()
+                        .stream()
+                        .filter(ap -> ap.getEdgeUser() != null)
+                        .findFirst()
+                        .get();
+                DENOPTIMAttachmentPoint trgAP = trgVertex
+                        .getAttachmentPoints()
+                        .stream()
+                        .filter(ap -> ap.getEdgeUser() != null)
+                        .findFirst()
+                        .get();
+
+                try {
+                    g.appendVertexOnAP(srcAP, trgAP);
+                } catch (DENOPTIMException e) {
+                    // Should not happen
+                    e.printStackTrace();
+                }
+            }
+        }
+        return g;
+    }
+
+//------------------------------------------------------------------------------
+
+    private void addRings(List<DENOPTIMVertex> vertices, DENOPTIMGraph g) {
+        List<List<DENOPTIMVertex>> ringVertices = Stream.of(
+                Arrays.asList(0, 2, 3, 4),
+                Arrays.asList(5, 2, 3, 4),
+                Arrays.asList(10, 12, 11),
+                Arrays.asList(9, 8, 11))
+                .map(indices -> indices
+                        .stream()
+                        .map(vertices::get)
+                        .collect(Collectors.toList())
+                )
+                .collect(Collectors.toList());
+
+        for (List<DENOPTIMVertex> vs : ringVertices) {
+            DENOPTIMRing r = new DENOPTIMRing();
+            for (DENOPTIMVertex v : vs) {
+                r.addVertex(v);
+            }
+            g.addRing(r);
+        }
+    }
+
+//------------------------------------------------------------------------------
+
+    private Set<DENOPTIMGraph> getExpectedSubgraphs(
+            List<DENOPTIMVertex> vertices, DENOPTIMGraph g) {
+        Set<DENOPTIMGraph> expectedSubgraphs = new HashSet<>();
+
+        List<Set<Integer>> keepVertices = Stream.of(
+                Stream.of(0, 2, 3, 4, 5),
+                Stream.of(8, 9, 10, 11, 12))
+                .map(indices -> indices.collect(Collectors.toSet()))
+                .collect(Collectors.toList());
+
+        for (Set<Integer> keepVertex : keepVertices) {
+            DENOPTIMGraph expSubgraph = g.clone();
+            Set<DENOPTIMVertex> removeVertices = IntStream
+                    .range(0, vertices.size())
+                    .filter(i -> !keepVertex.contains(i))
+                    .mapToObj(vertices::get)
+                    .collect(Collectors.toSet());
+
+            for (DENOPTIMVertex removeVertex : removeVertices) {
+                expSubgraph.removeVertex(removeVertex);
+            }
+            expectedSubgraphs.add(expSubgraph);
+        }
+        return expectedSubgraphs;
+    }
+
+//------------------------------------------------------------------------------
+
+    private DENOPTIMVertex buildFragment(String elementSymbol, int apCount) {
+        try {
+            IAtomContainer atomContainer = chemBuilder.newAtomContainer();
+            IAtom oxygen = chemBuilder.newAtom();
+            oxygen.setSymbol(elementSymbol);
+            atomContainer.addAtom(oxygen);
+
+            DENOPTIMFragment v = new DENOPTIMFragment(-1, atomContainer,
+                    BBType.FRAGMENT);
+            for (int i = 0; i < apCount; i++) {
+                v.addAP(0, DEFAULT_APCLASS, getRandomVector(), 1);
+            }
+            return v;
+        } catch (DENOPTIMException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//------------------------------------------------------------------------------
+
+    private Point3d getRandomVector() {
+        double precision = 10*10*10*10;
+        return new Point3d(
+                (double) (Math.round(rng.nextDouble() * precision)) / precision,
+                (double) (Math.round(rng.nextDouble() * precision)) / precision,
+                (double) (Math.round(rng.nextDouble() * precision)) / precision
+        );
+    }
+
+//------------------------------------------------------------------------------
+
     /**
      * Returns a 3-cycle. The S marks the scaffold vertex:
      *    /--- S ---\
@@ -221,7 +467,7 @@ public class DENOPTIMGraphOperationsTest {
 
         g.appendGraphOnAP(arm, 0, ring, ringScaffold,
                 ringScaffold.getNumberOfAPs() - 1,
-                DENOPTIMEdge.BondType.SINGLE, new HashMap<>());
+                BondType.SINGLE, new HashMap<>());
 
         DENOPTIMGraph.setScaffold(arm);
 
@@ -252,7 +498,7 @@ public class DENOPTIMGraphOperationsTest {
 
         cycle3.appendGraphOnGraph(scaff, scaff.getNumberOfAPs() - 1, cycle4,
                 connectTo, connectTo.getNumberOfAPs() - 1,
-                DENOPTIMEdge.BondType.SINGLE, new HashMap<>(), false);
+                BondType.SINGLE, new HashMap<>(), false);
 
         DENOPTIMGraph.setScaffold(scaff);
 
@@ -317,4 +563,44 @@ public class DENOPTIMGraphOperationsTest {
     }
 
 //------------------------------------------------------------------------------
+
+    private static final class ExtractPatternCase {
+        final DENOPTIMGraph g;
+        final int expectedSize;
+        final Set<DENOPTIMGraph> expectedGraphs;
+        final Comparator<DENOPTIMGraph> graphComparator = (gA, gB) ->
+                gA.sameAs(gB, new StringBuilder()) ? 0 : -1;
+
+        private ExtractPatternCase(DENOPTIMGraph g, int expectedSize,
+                                         Set<DENOPTIMGraph> expectedGraphs) {
+            this.g = g;
+            this.expectedSize = expectedSize;
+            this.expectedGraphs = expectedGraphs;
+        }
+
+        private boolean matchesExpected(Collection<DENOPTIMGraph> actual) {
+            if (actual.size() != expectedSize) {
+                return false;
+            }
+
+            Set<DENOPTIMGraph> actualsRemoved = new HashSet<>(expectedGraphs);
+            for (DENOPTIMGraph g : actual) {
+                if (expectedGraphs.stream().anyMatch(exp ->
+                        graphComparator.compare(g, exp) == 0)) {
+                    actualsRemoved = actualsRemoved
+                            .stream()
+                            .filter(exp -> graphComparator.compare(g, exp) == 0)
+                            .collect(Collectors.toSet());
+                } else {
+                    return false;
+                }
+            }
+
+            // Check that no graphs are missing from actual
+            return actualsRemoved.size() == 0;
+        }
+    }
+
+//------------------------------------------------------------------------------
+
 }
