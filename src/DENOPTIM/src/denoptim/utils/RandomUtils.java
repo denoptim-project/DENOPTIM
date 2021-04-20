97e564949e90578d870974427d287edd24928102
Einar Kjellback <einarkjellback@fastmail.com>
2021-04-20 12:09:57 +0200

    Fixed bug that produced a NullpointerException

--- src/DENOPTIM/src/denoptim/utils/RandomUtils.java
+++ src/DENOPTIM/src/denoptim/utils/RandomUtils.java
@@ -83,10 +83,7 @@ public class RandomUtils
     
     public static <T> T randomlyChooseOne(Collection<T> c)
     {
-        if (mt == null) {
-            initialiseRNG();
-        }
-        int chosen = mt.nextInt(c.size());
+        int chosen = getRNG().nextInt(c.size());
         int i=0;
         T chosenObj = null;
         for (T o : c)
