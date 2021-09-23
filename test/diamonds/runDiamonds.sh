3c927ee1ef700073b0d4bbfb5ab0b7af11186075
Marco Foscato <marco.foscato@uib.no>
2021-09-23 14:15:21 +0200

    adding diamond's test case

---
+++ test/diamonds/runDiamonds.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Script lauching an evolutionary experiment aiming to desing a diamond-like molecule.
+#
+
+# Setting the environment
+export SHELL="/bin/bash"
+export DENOPTIM_HOME="$(cd ../.. ; pwd)"
+export javaDENOPTIM="java"
+export DENOPTIMJarFiles="$DENOPTIM_HOME/build"
+if [ ! -f "$DENOPTIMJarFiles/DenoptimGA.jar" ]
+then
+    echo "Cannot find  $DENOPTIMJarFiles/DenoptimGA.jar"
+    echo "Trying under dist folder"
+    if [ ! -f "$DENOPTIMJarFiles/dist/DenoptimGA.jar" ]
+    then
+       echo "ERROR! Cannot find  $DENOPTIMJarFiles/dist/DenoptimGA.jar"
+       exit -1
+    fi
+    export DENOPTIMJarFiles="$DENOPTIM_HOME/build/dist"
+fi
+echo "Using DENOPTIM from $DENOPTIMJarFiles"
+
+# Copy to tmp space
+wDir="/tmp/denoptim_Diamond"
+if [ -d "$wDir" ]
+then
+    echo " "
+    echo "ERROR! Old $wDir exists already! Remove it to run a new test."
+    echo " "
+    exit
+fi
+mkdir "$wDir"
+if [ ! -d "$wDir" ]
+then
+    echo " "
+    echo "ERROR! Unable to create working directory $wDir"
+    echo " "
+    exit
+fi
+echo "Copying file to $wDir"
+cp -r * "$wDir"
+cd "$wDir"
+
+# Run DENOPTIM	
+echo " "
+echo "Starting DenoptimGA (ctrl+c to kill)"
+echo " "
+java -jar "$DENOPTIMJarFiles/DenoptimGA.jar" input_parameters
+
+# Goodbye
+echo "All done. See results under $wDir"
+echo "Thanks for using DENOPTIM!"
+
