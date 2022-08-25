d17816571c9c54963e279c69aa2ba8cea43bc3a0
Marco Foscato <marco.foscato@uib.no>
2022-08-25 15:24:23 +0200

    move Py4J server to integration package

--- src/main/java/denoptim/integration/python/Py4JGetawayServer.java
+++ src/main/java/denoptim/integration/python/Py4JGetawayServer.java
@@ -16,7 +16,7 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package denoptim.programs.deamon;
+package denoptim.integration.python;
 import denoptim.io.DenoptimIO;
 
 import py4j.GatewayServer;
