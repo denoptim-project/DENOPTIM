afb9aea8ce7153031f965243eec91cbbbf05ebb3
Marco Foscato <marco.foscato@uib.no>
2023-02-28 17:40:50 +0100

    removing user dependency

--- test/functional_tests/runTests.sh
+++ test/functional_tests/runTests.sh
@@ -23,7 +23,7 @@
 
 # Path to TINKER executables
 # Currently required only by t1 that will be skipped if the following is empty.
-export tinkerPathDENOPTIM="/Users/marcof/tools/tinkerLFMM_RCP_2/bin/"
+export tinkerPathDENOPTIM=""
 
 # Directory created for running tests (must be shorter than 40 characters).
 wDir="/tmp/denoptim_test"
