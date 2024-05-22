cbc27a0941758c1844288587cbdd33261f9d5302
Marcello Costamagna <marcello.costamagna@uib.no>
2024-05-22 13:57:55 +0200

    updates unit test connected to symmetry detection functionality

--- src/main/java/denoptim/graph/DGraph.java
+++ src/main/java/denoptim/graph/DGraph.java
@@ -36,7 +36,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.logging.Level;
