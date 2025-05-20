37c9ede80668fa5725921de87dac7db51db4435f
Marco Foscato <marco.foscato@uib.no>
2025-05-20 17:05:39 +0200

    setting JAVA_HOME for Windows runner

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,5 +1,23 @@
 cd "%SRC_DIR%"
 
+set "JAVA_HOME=%CONDA_PREFIX%\Library"
+
+if not exist "%JAVA_HOME%\bin\java.exe" (
+  echo Java not found in %JAVA_HOME%\bin
+  echo Searching for Java...
+  
+  if exist "%CONDA_PREFIX%\bin\java.exe" (
+    set "JAVA_HOME=%CONDA_PREFIX%"
+    echo Found Java in %JAVA_HOME%
+  ) else (
+    echo ERROR: Java not found in Conda environment
+    exit 1
+  )
+)
+
+echo Using JAVA_HOME: %JAVA_HOME%
+"%JAVA_HOME%\bin\java" -version
+
 cmd.exe /c mvn -version
 cmd.exe /c mvn --batch-mode clean || echo ""
 cmd.exe /c mvn --batch-mode package || echo ""
